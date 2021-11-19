e1c39395d5c2d795c87e008a466d5ed197239c8d
ajsarmah <thisisamlan49@gmail.com>
2021-11-19 16:42:21 +0530

    added functions to start and stop test and calculation of WPM .

--- app.js
+++ app.js
@@ -61,5 +61,38 @@ const paragraph = [
   "previous",
   "yelling.",
 ];
+//global variables
 const textArea = document.getElementById("t-area");
 const startButton = document.getElementById("start-btn");
+const displayTimer = document.getElementById("time");
+let timer = 10;
+let interval;
+//app functions
+const extractTextAreaValue = () => {
+  textArea.setAttribute("disabled", "true");
+  const userText = textArea.value;
+  let userTextArray = userText.split(" ");
+  userTextArray = userTextArray.filter((element) => {
+    return element.length > 1;
+  });
+  return userTextArray;
+};
+const getWordPerMin = (input) => {
+  return input.length;
+};
+//interval function
+const startTest = () => {
+  startButton.classList.add("disabled");
+  textArea.removeAttribute("disabled");
+  interval = setInterval(() => {
+    timer = timer - 1;
+    displayTimer.innerHTML = timer;
+    if (timer === 0) {
+      clearInterval(interval);
+      userInput = extractTextAreaValue();
+      WPM = getWordPerMin(userInput);
+      console.log(WPM);
+    }
+  }, 1000);
+};
+startButton.addEventListener("click", startTest);
